82aa7d996a4fab2f7879e79c6ef5000758908b4f
sergey.dolgozvjaga <serg9888@gmail.com>
2019-05-06 15:32:20 +0300

    add all files

---
+++ ReadFromXml/XmlParser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+
+namespace ReadFromXml
+{
+    public class XmlParser
+    {
+        // files
+        private const string fileUrl = "URL.xml";
+        private const string fileFindElements = "FindElements.xml";
+
+        // tables
+        private const string fileUrlTableUrl = "/Table/URL";
+        private const string fileFindElementsTableStartPageXPath = "/Table/Start_page/XPath";
+
+
+        private XmlNode LoadXmlFileAndXmlTable(string file, string path)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(file);
+            XmlNode node = xmlDoc.DocumentElement.SelectSingleNode(path);
+            return node;
+        }
+
+        public XmlNode LoadFileUrlTableUrl()
+        {
+            return LoadXmlFileAndXmlTable(fileUrl, fileUrlTableUrl);
+        }
+
+        public XmlNode LoadFileFindElementsTableStartPageXPath()
+        {
+            return LoadXmlFileAndXmlTable(fileFindElements, fileFindElementsTableStartPageXPath);
+        }
+    }
+}
---
+++ ReadFromXml/driver/Driver.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace ReadFromXml
+{
+    public class Driver
+    {
+        public IWebDriver driver;
+
+        public void SetUpFireFoxDriver()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public void SetUpChromeDriver()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public void NavigatePage(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public string GetURL()
+        {
+            return driver.Url;
+        }
+
+        public void Close()
+        {            
+            driver.Quit();
+        }
+    }
+}
